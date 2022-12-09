32211423d5c8e36ecf26ad83ac9acba6098f5a83
cepix1234 <bocepek027@gmail.com>
2022-12-09 21:32:11 +0100

    add todo

--- PhoneNumberLookup/Initialization/Initialization.cs
+++ PhoneNumberLookup/Initialization/Initialization.cs
@@ -5,6 +5,8 @@ namespace PhoneNumberLookup.Initialization
     { 
         public static void _Initialize()
         {
+            //TODO get api key from config file provided by user
+            //if no config file found init file with input for api key
             NumberLookupControler._ApiEndpoint = "https://ipqualityscore.com/api/json";
             NumberLookupControler._ApiKey = "YrmOmwQ0njdMWNGNtXCunBsGKlL6sevB";
         }
